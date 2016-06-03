1ad157add2666147b7da8cc58197d37abeb54c1b
Stephanie Gurung <stephr3@gmail.com>
2016-06-03 10:08:39 -0700

    add and test tally for all questions

--- js/scripts.js
+++ js/scripts.js
@@ -20,12 +20,42 @@ $(document).ready(function(){
       sanFrancisco += 5;
     }
 
-    // if(question2 = "budget"){
-    //   staycation += 5;
-    //   seattle += 2;
-    // } else if (question2 = "money"){
-    //   sanFrancisco += 5;
-    // }
+    if(question2 === "budget"){
+      staycation += 5;
+      seattle += 2;
+    } else if (question2 === "money"){
+      sanFrancisco += 5;
+    }
+
+    if(question3 === "ownBed"){
+      staycation += 5;
+    } else if (question3 === "hotel"){
+      sanFrancisco += 2;
+      seattle += 1;
+    }
+
+    if(question4 === "river"){
+      staycation += 5;
+    } else if (question4 === "sea"){
+      sanFrancisco += 2;
+      seattle += 1;
+    }
+
+    if(question5 === "spaceNeedle"){
+      seattle += 5;
+    } else if (question5 === "bridges"){
+      sanFrancisco += 3;
+      staycation += 2;
+    }
+
+    if(question6 === "starbucks"){
+      seattle += 5;
+    } else if (question6 === "otherCoffee"){
+      sanFrancisco += 2;
+      staycation += 1;
+    }
+
+
     alert(["staycation: " + staycation, "seattle: " + seattle, "sanFrancisco: " + sanFrancisco, "q1:" + question1, "q2:"+question2, "q3:"+question3, "q4:"+question4, "q5:" +question5, "q6:"+question6]);
   });
 });
