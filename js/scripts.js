c4248602b357ceaa0e40cc05c9f672dac88dc9d6
Stephanie Gurung <stephr3@gmail.com>
2016-06-03 09:45:21 -0700

    add results tally variables and functionality to increase tally for each question

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
-    alert("submit button pushed!");
     event.preventDefault();
     var question1 = $("#question1").val();
     var question2 = $("#question2").val();
@@ -8,7 +7,18 @@ $(document).ready(function(){
     var question4 = $("#question4").val();
     var question5 = $("#question5").val();
     var question6 = $("#question6").val();
+
+    var staycation = 0;
+    var seattle = 0;
+    var sanFrancisco = 0;
     var result;
-    alert([question1,question2, question3, question4, question5, question6]);
+
+    if(question1 = "car"){
+      staycation += 1;
+      seattle += 2;
+    } else if (question1 = "fly"){
+      sanFrancisco += 5;
+    }
+    alert(["staycation: " + staycation, "seattle: " + seattle, "sanFrancisco: " + sanFrancisco]);
   });
 });
